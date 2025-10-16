9ab5982f07a02ab0803ce5cc9b0825c658fcf464
Mine <air1345@gmail.com>
2025-10-16 14:00:19 +0500

    Update user.js

--- user.js
+++ user.js
@@ -5,5 +5,12 @@ const users = [
   { username: "Mohamed", name: "Mohamed Ibrahim", passwordHash: "c75f28325cfa028ea13872f977a29e0e87c99a4f390fe260f24d7e1f05fb8d75", Level: "Supervisor" },
   { username: "fiyaza", name: "Fiyaza Abdulla", passwordHash: "ca358cbdaddb39c71130153049e7bc96f387b1f0030deac24841c74f269831ed", Level: "User" },
   { username: "A-6313", name: "Aminath Almas", passwordHash: "ca8f5d7677b1bfb7ce14d15ca2f464f46c35e87fddc1409537e4aab52ba3991b", Level: "User" },
+  { username: "Jeezaan", name: "Mohamed Jeezaan", passwordHash: "3b3f52f6ba4bac4ed550535531c0e5850a87c4eb87c3a1dab0bb423cf27d7de0", Level: "User" },
+  { username: "Tholal", name: "Ahmed Tholal", passwordHash: "f379c132f3eb4877b078e7fcdd272b24e2010215aa91dc0c1682add172f33767", Level: "User" },
+  { username: "Hassan", name: "Hassan Haneef", passwordHash: "aee651be73c9084e024798b3b10ac19ccbf2a905e70571118dd757fc4c99e54a", Level: "User" },
+  { username: "Mujahid", name: "Mohamed Mujahid", passwordHash: "38b3361fff128a49da6982886a40f7a12f501f0bd861ec49325f267f7587b7e8", Level: "User" },
+  { username: "Fazana", name: "Fazana Ali", passwordHash: "fac1bcc6b16f45f91403cb857dfcf628f13f94998e70c3d87ed906b324e87a9c", Level: "User" },
+  { username: "Fazeel", name: "Adam Fazeel", passwordHash: "8e71b24534e9f3fb3a71263359fed2b7ffb008265e0d34383e319f1b6f5c08f2", Level: "User" },
+  { username: "Shafaq", name: "Aishath Shafaq", passwordHash: "6b1f4a8bb8bd2821ba8c92098a5711c3cdd04ac70f93f8fe9787c3f8bb1c56b9", Level: "User" },
   
 ];
